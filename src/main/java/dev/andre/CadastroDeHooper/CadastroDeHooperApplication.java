26cdfe9442f225e36d0386209529fa9652202cc0
andrehercules <andresilvapereira200@gmail.com>
2026-02-01 18:05:21 -0300

    first commit

---
+++ src/main/java/dev/andre/CadastroDeHooper/CadastroDeHooperApplication.java
@@ -0,0 +1,13 @@
+package dev.andre.CadastroDeHooper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeHooperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeHooperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/andre/CadastroDeHooper/CadastroDeHooperApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.andre.CadastroDeHooper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ACadastroDeHooperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
